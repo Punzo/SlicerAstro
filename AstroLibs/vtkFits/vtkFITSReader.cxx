21c4d3721c4e5bbf35e631feb2c14609e24eb339
Davide Punzo <punzodavide@hotmail.it>
2015-10-23 16:22:07 +0200

    BUG: vtkFitsReader was using StringToFloat for double value

--- AstroLibs/vtkFits/vtkFITSReader.cxx
+++ AstroLibs/vtkFits/vtkFITSReader.cxx
@@ -352,9 +352,9 @@ void vtkFITSReader::ExecuteInformation()
     }
 
 
-  float theta1 = (StringtoDouble(this->GetHeaderValue("SlicerAstro.CDELT1")) > 0.) ? 0. : M_PI;
-  float theta2 = (StringtoDouble(this->GetHeaderValue("SlicerAstro.CDELT2")) > 0.) ? 0. : M_PI;
-  float theta3 = (StringtoDouble(this->GetHeaderValue("SlicerAstro.CDELT3")) > 0.) ? 0. : M_PI;
+  double theta1 = (StringToDouble(this->GetHeaderValue("SlicerAstro.CDELT1")) > 0.) ? 0. : M_PI;
+  double theta2 = (StringToDouble(this->GetHeaderValue("SlicerAstro.CDELT2")) > 0.) ? 0. : M_PI;
+  double theta3 = (StringToDouble(this->GetHeaderValue("SlicerAstro.CDELT3")) > 0.) ? 0. : M_PI;
   theta1 += M_PI/2.;
 
   this->RasToIjkMatrix->SetElement(0, 0, cos(theta2) * cos(theta3));
