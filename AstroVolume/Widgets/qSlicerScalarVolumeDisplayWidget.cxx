9472139c20e0e9d0dc592401d294d8e02586532a
Davide <punzodavide@hotmail.it>
2015-12-10 12:29:27 +0100

    BUG: restore file

--- AstroVolume/Widgets/qSlicerScalarVolumeDisplayWidget.cxx
+++ AstroVolume/Widgets/qSlicerScalarVolumeDisplayWidget.cxx
@@ -1,134 +1,143 @@
-/// Qt includes
-#include <QFileInfo>
+// QT includes
 
-// CTK includes
-#include <ctkFlowLayout.h>
-#include <ctkUtils.h>
+// SlicerQT includes
+#include "qSlicerLabelMapVolumeDisplayWidget.h"
+#include "qSlicerScalarVolumeDisplayWidget.h"
+#include "qSlicerAstroVolumeDisplayWidget.h"
 
-// AstroVolume includes
-#include "qSlicerIOOptions_p.h"
-#include "qSlicerAstroVolumeIOOptionsWidget.h"
-#include "ui_qSlicerAstroVolumeIOOptionsWidget.h"
+// MRML includes
+#include <vtkMRMLVolumeNode.h>
+#include <vtkMRMLAstroVolumeNode.h>
+#include <vtkMRMLAstroLabelMapVolumeNode.h>
 
 //-----------------------------------------------------------------------------
 /// \ingroup Slicer_QtModules_AstroVolume
-class qSlicerAstroVolumeIOOptionsWidgetPrivate
-  : public qSlicerIOOptionsPrivate
-  , public Ui_qSlicerAstroVolumeIOOptionsWidget
+class qSlicerAstroVolumeDisplayWidgetPrivate
 {
+  Q_DECLARE_PUBLIC(qSlicerAstroVolumeDisplayWidget);
+
+protected:
+  qSlicerAstroVolumeDisplayWidget* const q_ptr;
+
 public:
+  qSlicerAstroVolumeDisplayWidgetPrivate(qSlicerAstroVolumeDisplayWidget& object);
+  void init();
+  void setCurrentDisplayWidget(qSlicerWidget* displayWidget);
+
+  qSlicerScalarVolumeDisplayWidget*            ScalarVolumeDisplayWidget;
+  qSlicerLabelMapVolumeDisplayWidget*          LabelMapVolumeDisplayWidget;
 };
 
-//-----------------------------------------------------------------------------
-qSlicerAstroVolumeIOOptionsWidget::qSlicerAstroVolumeIOOptionsWidget(QWidget* parentWidget)
-  : qSlicerIOOptionsWidget(new qSlicerAstroVolumeIOOptionsWidgetPrivate, parentWidget)
+// --------------------------------------------------------------------------
+qSlicerAstroVolumeDisplayWidgetPrivate::qSlicerAstroVolumeDisplayWidgetPrivate(
+  qSlicerAstroVolumeDisplayWidget& object)
+  : q_ptr(&object)
 {
-  Q_D(qSlicerAstroVolumeIOOptionsWidget);
-  d->setupUi(this);
-
-  ctkFlowLayout::replaceLayout(this);
-  /*
-  // Replace the horizontal layout with a flow layout
-  ctkFlowLayout* flowLayout = new ctkFlowLayout;
-  flowLayout->setPreferredExpandingDirections(Qt::Horizontal);
-  flowLayout->setAlignItems(false);
-  QLayout* oldLayout = this->layout();
-  int margins[4];
-  oldLayout->getContentsMargins(&margins[0],&margins[1],&margins[2],&margins[3]);
-  QLayoutItem* item = 0;
-  while((item = oldLayout->takeAt(0)))
-    {
-    if (item->widget())
-      {
-      flowLayout->addWidget(item->widget());
-      }
-    }
-  // setLayout() will take care or reparenting layouts and widgets
-  delete oldLayout;
-  flowLayout->setContentsMargins(0,0,0,0);
-  this->setLayout(flowLayout);
-  */
-
-  connect(d->NameLineEdit, SIGNAL(textChanged(QString)),
-          this, SLOT(updateProperties()));
-  connect(d->LabelMapCheckBox, SIGNAL(toggled(bool)),
-          this, SLOT(updateProperties()));
-  connect(d->CenteredCheckBox, SIGNAL(toggled(bool)),
-          this, SLOT(updateProperties()));
-  connect(d->SingleFileCheckBox, SIGNAL(toggled(bool)),
-          this, SLOT(updateProperties()));
-
-  // Single file by default
-  d->SingleFileCheckBox->setChecked(true);
+  this->ScalarVolumeDisplayWidget = 0;
+  this->LabelMapVolumeDisplayWidget = 0;
 }
 
-//-----------------------------------------------------------------------------
-qSlicerAstroVolumeIOOptionsWidget::~qSlicerAstroVolumeIOOptionsWidget()
+// --------------------------------------------------------------------------
+void qSlicerAstroVolumeDisplayWidgetPrivate::init()
+{
+  Q_Q(qSlicerAstroVolumeDisplayWidget);
+  this->ScalarVolumeDisplayWidget = new qSlicerScalarVolumeDisplayWidget(q);
+  q->addWidget(this->ScalarVolumeDisplayWidget);
+
+  this->LabelMapVolumeDisplayWidget = new qSlicerLabelMapVolumeDisplayWidget(q);
+  q->addWidget(this->LabelMapVolumeDisplayWidget);
+}
+
+// --------------------------------------------------------------------------
+void qSlicerAstroVolumeDisplayWidgetPrivate::setCurrentDisplayWidget(
+  qSlicerWidget* displayWidget)
+{
+  Q_Q(qSlicerAstroVolumeDisplayWidget);
+  qSlicerWidget* activeWidget = qobject_cast<qSlicerWidget*>(q->currentWidget());
+  if (activeWidget == displayWidget)
+    {
+    return;
+    }
+  if (activeWidget)
+    {
+    // We must remove the node "before" the setting the scene to 0.
+    // Because removing the scene could modify the observed node (e.g setting
+    // the scene to 0 on a colortable combobox will set the color node of the
+    // observed node to 0.
+    vtkMRMLNode* emptyVolumeNode = 0;
+    if (activeWidget == this->ScalarVolumeDisplayWidget)
+      {
+      this->ScalarVolumeDisplayWidget->setMRMLVolumeNode(emptyVolumeNode);
+      }
+
+    if (activeWidget == this->LabelMapVolumeDisplayWidget)
+      {
+      this->LabelMapVolumeDisplayWidget->setMRMLVolumeNode(emptyVolumeNode);
+      }
+    activeWidget->setMRMLScene(0);
+  }
+  // QStackWidget::setCurrentWidget(0) is not supported
+  if (displayWidget)
+    {
+    q->setCurrentWidget(displayWidget);
+    }
+}
+
+// --------------------------------------------------------------------------
+// qSlicerAstroVolumeDisplayWidget
+// --------------------------------------------------------------------------
+qSlicerAstroVolumeDisplayWidget::qSlicerAstroVolumeDisplayWidget(QWidget* parentWidget)
+  : Superclass(parentWidget)
+  , d_ptr(new qSlicerAstroVolumeDisplayWidgetPrivate(*this))
+{
+  Q_D(qSlicerAstroVolumeDisplayWidget);
+  d->init();
+}
+
+// --------------------------------------------------------------------------
+qSlicerAstroVolumeDisplayWidget::~qSlicerAstroVolumeDisplayWidget()
 {
 }
 
-//-----------------------------------------------------------------------------
-void qSlicerAstroVolumeIOOptionsWidget::updateProperties()
+// --------------------------------------------------------------------------
+void qSlicerAstroVolumeDisplayWidget::setMRMLVolumeNode(vtkMRMLNode* VolumeNode)
 {
-  Q_D(qSlicerAstroVolumeIOOptionsWidget);
-  if (!d->NameLineEdit->text().isEmpty())
+   Q_D(qSlicerAstroVolumeDisplayWidget);
+   qvtkDisconnect(0, vtkCommand::ModifiedEvent,
+                  this, SLOT(updateFromMRML(vtkObject*)));
+
+  if (VolumeNode == 0)
     {
-    QStringList names = d->NameLineEdit->text().split(';');
-    for (int i = 0; i < names.count(); ++i)
-      {
-      names[i] = names[i].trimmed();
-      }
-    d->Properties["name"] = names;
+    d->setCurrentDisplayWidget(0);
+    return;
     }
-  else
+
+  vtkMRMLScene* scene = VolumeNode->GetScene();
+  vtkMRMLAstroLabelMapVolumeNode* labelMapVolumeNode =
+    vtkMRMLAstroLabelMapVolumeNode::SafeDownCast(VolumeNode);
+  vtkMRMLAstroVolumeNode* astroVolumeNode =
+    vtkMRMLAstroVolumeNode::SafeDownCast(VolumeNode);
+  if (astroVolumeNode)
     {
-    d->Properties.remove("name");
+    qvtkConnect(VolumeNode, vtkCommand::ModifiedEvent,
+              this, SLOT(updateFromMRML(vtkObject*)));
+    d->ScalarVolumeDisplayWidget->setMRMLScene(scene);
+    d->ScalarVolumeDisplayWidget->setMRMLVolumeNode(VolumeNode);
+    d->setCurrentDisplayWidget(d->ScalarVolumeDisplayWidget);
     }
-  d->Properties["labelmap"] = d->LabelMapCheckBox->isChecked();
-  d->Properties["center"] = d->CenteredCheckBox->isChecked();
-  d->Properties["singleFile"] = d->SingleFileCheckBox->isChecked();
+  else if (labelMapVolumeNode)
+   {
+   qvtkConnect(VolumeNode, vtkCommand::ModifiedEvent,
+             this, SLOT(updateFromMRML(vtkObject*)));
+   d->LabelMapVolumeDisplayWidget->setMRMLScene(scene);
+   d->LabelMapVolumeDisplayWidget->setMRMLVolumeNode(VolumeNode);
+   d->setCurrentDisplayWidget(d->LabelMapVolumeDisplayWidget);
+   }
 }
 
-//-----------------------------------------------------------------------------
-void qSlicerAstroVolumeIOOptionsWidget::setFileName(const QString& fileName)
+// --------------------------------------------------------------------------
+void qSlicerAstroVolumeDisplayWidget::updateFromMRML(vtkObject* Volume)
 {
-  this->setFileNames(QStringList(fileName));
-}
-
-//-----------------------------------------------------------------------------
-void qSlicerAstroVolumeIOOptionsWidget::setFileNames(const QStringList& fileNames)
-{
-  Q_D(qSlicerAstroVolumeIOOptionsWidget);
-  QStringList names;
-  bool onlyNumberInName = false;
-  bool onlyNumberInExtension = false;
-  bool hasLabelMapName = false;
-  foreach(const QString& fileName, fileNames)
-    {
-    QFileInfo fileInfo(fileName);
-    if (fileInfo.isFile())
-      {
-      names << fileInfo.completeBaseName();
-      // Single file
-      // If the name (or the extension) is just a number, then it must be a 2D
-      // slice from a 3D volume, so uncheck Single File.
-      onlyNumberInName = QRegExp("[0-9\\.\\-\\_\\@\\(\\)\\~]+").exactMatch(fileInfo.baseName());
-      fileInfo.suffix().toInt(&onlyNumberInExtension);
-      }
-    // Because '_' is considered as a word character (\w), \b
-    // doesn't consider '_' as a word boundary.
-    QRegExp labelMapName("(\\b|_)([Ll]abel(s)?)(\\b|_)");
-    QRegExp segName("(\\b|_)([Ss]eg)(\\b|_)");
-    QRegExp maskName("(\\b|_)([Mm]ask)(\\b|_)");
-    if (fileInfo.baseName().contains(labelMapName) ||
-        fileInfo.baseName().contains(segName) ||
-        fileInfo.baseName().contains(maskName))
-      {
-      hasLabelMapName = true;
-      }
-    }
-  d->NameLineEdit->setText( names.join("; ") );
-  d->SingleFileCheckBox->setChecked(!onlyNumberInName && !onlyNumberInExtension);
-  d->LabelMapCheckBox->setChecked(hasLabelMapName);
-  this->qSlicerIOOptionsWidget::setFileNames(fileNames);
+  vtkMRMLVolumeNode* VolumeNode = vtkMRMLVolumeNode::SafeDownCast(Volume);
+  this->setMRMLVolumeNode(VolumeNode);
 }
