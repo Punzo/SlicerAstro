47649fbfdf90d06a0cc85f33c11e997f40cef590
Davide Punzo <punzodavide@hotmail.it>
2016-11-30 16:45:22 +0100

    ENH: updating WEIN069_mask download link

--- AstroSampleData/AstroSampleData.py
+++ AstroSampleData/AstroSampleData.py
@@ -168,7 +168,7 @@ class AstroSampleDataLogic:
     """Fills in the pre-define sample data sources"""
     sourceArguments = (
         ('WEIN069', 'http://slicer.kitware.com/midas3/download/item/266401/WEIN069.fits', 'WEIN069.fits', 'WEIN069'),
-        ('WEIN069_MASK', 'http://slicer.kitware.com/midas3/download/item/258743/WEIN069_mask.fits', 'WEIN069_mask.fits', 'WEIN069_mask'),
+        ('WEIN069_MASK', 'http://slicer.kitware.com/midas3/download/item/266402/WEIN069_mask.fits', 'WEIN069_mask.fits', 'WEIN069_mask'),
         ('NGC2403', 'http://slicer.kitware.com/midas3/download/item/242878/NGC2403.fits', 'NGC2403.fits', 'NGC2403'),
         ('NGC4111', 'http://slicer.kitware.com/midas3/download/item/242880/NGC4111.fits', 'NGC4111.fits', 'NGC4111'),
         ('NGC3379', 'http://slicer.kitware.com/midas3/download/item/242866/NGC3379.fits', 'NGC3379.fits', 'NGC3379'),
