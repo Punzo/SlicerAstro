cde6f528f48ace77882c58c543902c5a07995c46
Davide Punzo <punzodavide@hotmail.it>
2017-05-03 17:30:16 +0200

    BUG: fixed url NGC2403

--- AstroSampleData/AstroSampleData.py
+++ AstroSampleData/AstroSampleData.py
@@ -169,7 +169,7 @@ class AstroSampleDataLogic:
     sourceArguments = (
         ('WEIN069', 'http://slicer.kitware.com/midas3/download/item/266401/WEIN069.fits', 'WEIN069.fits', 'WEIN069'),
         ('WEIN069_MASK', 'http://slicer.kitware.com/midas3/download/item/266403/WEIN069_mask.fits', 'WEIN069_mask.fits', 'WEIN069_mask'),
-        ('NGC2403', 'http://slicer.kitware.com/midas3/download/item/242878/NGC2403.fits', 'NGC2403.fits', 'NGC2403'),
+        ('NGC2403', 'http://slicer.kitware.com/midas3/download/item/287185/NGC2403.fits', 'NGC2403.fits', 'NGC2403'),
         ('NGC4111', 'http://slicer.kitware.com/midas3/download/item/242880/NGC4111.fits', 'NGC4111.fits', 'NGC4111'),
         ('NGC3379', 'http://slicer.kitware.com/midas3/download/item/242866/NGC3379.fits', 'NGC3379.fits', 'NGC3379'),
         )
