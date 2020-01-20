c41b3bdb7b80067f7c4bcc39395c09e99778c291
Davide Punzo <punzodavide@hotmail.it>
2020-01-20 16:13:27 +0100

    Re #106: fixing packing for astropy

--- SuperBuild/External_python-slicerastro-requirements.cmake
+++ SuperBuild/External_python-slicerastro-requirements.cmake
@@ -1,25 +1,35 @@
 set(proj python-slicerastro-requirements)
 
-# Set dependency list
-set(${proj}_DEPENDENCIES)
-
-# Include dependent projects if any
-ExternalProject_Include_Dependencies(${proj} PROJECT_VAR proj DEPENDS_VAR ${proj}_DEPENDENCIES)
+# Alternative python prefix for installing extension python packages
+set(python_packages_DIR "${CMAKE_BINARY_DIR}/python-packages-install")
+file(TO_NATIVE_PATH ${python_packages_DIR} python_packages_DIR_NATIVE_DIR)
 
 ExternalProject_Add(${proj}
   ${${proj}_EP_ARGS}
   DOWNLOAD_COMMAND ""
-  SOURCE_DIR ${CMAKE_BINARY_DIR}/${proj}
+  SOURCE_DIR ${python_packages_DIR}/${proj}
   BUILD_IN_SOURCE 1
   CONFIGURE_COMMAND ""
   BUILD_COMMAND ""
-  INSTALL_COMMAND ${PYTHON_EXECUTABLE} -m pip install -r ${SlicerAstro_SOURCE_DIR}/SuperBuild/slicerastro-requirements.txt
+  INSTALL_COMMAND ${PYTHON_EXECUTABLE} -m pip install -r ${SlicerAstro_SOURCE_DIR}/SuperBuild/slicerastro-requirements.txt --prefix ${python_packages_DIR_NATIVE_DIR}
   LOG_INSTALL 1
   DEPENDS
-    ${${proj}_DEPENDENCIES}
   )
 
 ExternalProject_GenerateProjectDescription_Step(${proj}
   VERSION ${_version}
   )
+
+# Launcher setting specific to build tree
+set(${proj}_PYTHONPATH_LAUNCHER_BUILD
+  ${python_packages_DIR}/${PYTHON_STDLIB_SUBDIR}
+  ${python_packages_DIR}/${PYTHON_STDLIB_SUBDIR}/lib-dynload
+  ${python_packages_DIR}/${PYTHON_SITE_PACKAGES_SUBDIR}
+  )
+mark_as_superbuild(
+  VARS ${proj}_PYTHONPATH_LAUNCHER_BUILD
+  LABELS "PYTHONPATH_LAUNCHER_BUILD"
+  )
+
+mark_as_superbuild(python_packages_DIR:PATH)
 
