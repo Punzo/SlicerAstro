1da6c116619f9ea65953e949874c2e83dd176198
Davide Punzo <punzodavide@hotmail.it>
2020-07-27 17:05:29 +0200

    Remove double icon from SlicerAstro welcome

--- SlicerAstroWelcome/qSlicerAstroWelcomeModuleWidget.cxx
+++ SlicerAstroWelcome/qSlicerAstroWelcomeModuleWidget.cxx
@@ -82,7 +82,7 @@ void qSlicerAstroWelcomeModuleWidgetPrivate::setupUi(qSlicerWidget* widget)
 
   this->Ui_qSlicerAstroWelcomeModuleWidget::setupUi(widget);
 
-  this->IconLabel->setPixmap(QPixmap(QLatin1String(":/Images/SlicerAstroIcon.png")));
+  //this->IconLabel->setPixmap(QPixmap(QLatin1String(":/Images/SlicerAstroIcon.png")));
   // Create the button group ensuring that only one collabsibleWidgetButton will be open at a time
   ctkButtonGroup * group = new ctkButtonGroup(widget);
 
